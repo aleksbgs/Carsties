534811fae14cccd3862b786e413613d5f4a18a2d
Aleksandar Markovic <amarkovic29@gmail.com>
2023-08-11 16:46:09 +0200

    fix

---
+++ src/BiddingService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,18 @@
+using System.Drawing;
+using AutoMapper;
+using BiddingService.DTOs;
+using BiddingService.Models;
+using Contracts;
+
+namespace BiddingService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+
+    public MappingProfiles()
+    {
+        CreateMap<Bid, BidDto>();
+        CreateMap<Bid, BidPlaced>();
+    }
+
+}
