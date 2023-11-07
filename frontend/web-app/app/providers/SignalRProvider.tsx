edbe988b761b23d8e9ac6c13ee4d53170b362f3e
Aleksandar Markovic <aleksandar.markovic@asseco-see.rs>
2023-11-07 17:01:09 +0100

    dfdf

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -21,7 +21,7 @@ export default function SignalRProvider({ children, user }: Props) {
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
     const apiUrl = process.env.NODE_ENV === 'production' //TODO: still getting error from somewhere here
-        ? 'https://api.auctions.store/notifications'
+        ? 'https://api.riomire.com/notifications'
         : process.env.NEXT_PUBLIC_NOTIFY_URL
 
     useEffect(() => {
